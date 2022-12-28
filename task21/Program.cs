f59c5c014860e4a0525ed568e6a3ccd304b13645
AlekseiEliseev <lexaelish@mail.ru>
2022-12-28 18:18:29 +0300

    Решил задачу 23 и 21 задачу

--- task21/Program.cs
+++ task21/Program.cs
@@ -5,9 +5,10 @@
 
 int Ax = Convert.ToInt32(Console.ReadLine());
 int Ay = Convert.ToInt32(Console.ReadLine());
+int Az = Convert.ToInt32(Console.ReadLine());
 int Bx = Convert.ToInt32(Console.ReadLine());
 int By = Convert.ToInt32(Console.ReadLine());
+int Bz = Convert.ToInt32(Console.ReadLine());
 
-
-double distance = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2)), 2);
+double distance = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2)), 2);
 Console.WriteLine(distance);
