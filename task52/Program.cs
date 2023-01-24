47d43a98c2a74b157b0c402b2f555740fb45a54a
AlekseiEliseev <lexaelish@mail.ru>
2023-01-25 00:28:37 +0300

    Решил задачу 47

---
+++ task52/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+// void ArithmeticMean(int[,] matrix, int m, int n)
+// {
+//     double sum = 0;
+//     for (int j = 0; j < matrix.Length; j++)
+//     {
+//         for (int i = 0; i < matrix.Length; i++)
+//         {
+//            sum += matrix[i, j];
+//         }
+//     }
+//     Console.WriteLine($"Среднее арифметическое каждого столбца: {sum / matrix.Length} ;");
+// }
+
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArrayWithRandom(matrix, m, n);
+// ArithmeticMean(matrix, m, n);
+
+Console.WriteLine(matrix.GetLength(0));
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{ sum / matrix.GetLength(0)} ");
+}
+Console.ReadLine();
